2e9bab249c8ba72cb0b5f3c53bbfcdbc01060b9d
Kasyanenko Danil <danildanilkas1@gmail.com>
2022-10-24 16:30:28 +0300

    init project

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,24 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
-}
+};
+
+export default registerRootComponent(App);
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
     justifyContent: 'center',
   },
 });
